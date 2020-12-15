6db24a84635554ae9891ecdd5f9094ca03855dd9
Kenouly Lo Van <kenouly.lovan@gmail.com>
2020-12-15 20:44:57 +0100

    turn db connection uri into env

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const cors = require('cors')
 
 // Connect to db
 mongoose
-  .connect('mongodb://localhost/what-s-in-my-fridge', {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
