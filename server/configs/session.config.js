bf778531bef3ef16db6e7ab23a4faf3152e366b1
Kenouly Lo Van <kenouly.lovan@gmail.com>
2020-12-19 11:40:05 +0100

    session config added

---
+++ server/configs/session.config.js
@@ -0,0 +1,19 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+
+module.exports = (app) => {
+  app.set('rust proxy', true);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 86400000, sameSite: 'none', secure: true },
+      store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 60 * 60 * 24,
+      }),
+    })
+  );
+};
